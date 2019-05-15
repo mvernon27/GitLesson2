843107de4d96cb6730717e869a52419578d8450e
mvernon27 <mvernon2705@gmail.com>
2019-05-15 11:33:04 -0400

    Added line

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace GitLesson2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Funky World!");
+            Console.WriteLine("Goodbye World");
         }
     }
 }
