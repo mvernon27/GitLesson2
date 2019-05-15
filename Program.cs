96558bb9e22f218bbd7ef0d87d1cb21df7850781
mega24cde <megan.denman@gmail.com>
2019-05-15 11:25:09 -0400

    Added 'funky'

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GitLesson2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Funky World!");
         }
     }
 }
